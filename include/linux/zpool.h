14bee44ebb67dcb09280d2a80ad1b1eeaecfc4d8
Dan Streetman <ddstreet@ieee.org>
2015-11-20 18:20:06 +0530

    Added support for zPool

--- include/linux/zpool.h
+++ include/linux/zpool.h
@@ -36,10 +36,19 @@ enum zpool_mapmode {
 	ZPOOL_MM_DEFAULT = ZPOOL_MM_RW
 };
 
+<<<<<<< HEAD
 struct zpool *zpool_create_pool(char *type, char *name,
 			gfp_t gfp, struct zpool_ops *ops);
 
 char *zpool_get_type(struct zpool *pool);
+=======
+bool zpool_has_pool(char *type);
+
+struct zpool *zpool_create_pool(const char *type, const char *name,
+			gfp_t gfp, const struct zpool_ops *ops);
+
+const char *zpool_get_type(struct zpool *pool);
+>>>>>>> b2e16ac... Added support for zPool
 
 void zpool_destroy_pool(struct zpool *pool);
 
@@ -81,7 +90,14 @@ struct zpool_driver {
 	atomic_t refcount;
 	struct list_head list;
 
+<<<<<<< HEAD
 	void *(*create)(char *name, gfp_t gfp, struct zpool_ops *ops);
+=======
+	void *(*create)(const char *name,
+			gfp_t gfp,
+			const struct zpool_ops *ops,
+			struct zpool *zpool);
+>>>>>>> b2e16ac... Added support for zPool
 	void (*destroy)(void *pool);
 
 	int (*malloc)(void *pool, size_t size, gfp_t gfp,
@@ -102,6 +118,11 @@ void zpool_register_driver(struct zpool_driver *driver);
 
 int zpool_unregister_driver(struct zpool_driver *driver);
 
+<<<<<<< HEAD
 int zpool_evict(void *pool, unsigned long handle);
 
 #endif
+=======
+#endif
+
+>>>>>>> b2e16ac... Added support for zPool
